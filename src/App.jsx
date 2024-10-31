883f4a79a80b6ade1815aa2b692b61c540d4d0cd
Walunj <pwalunj@ptc.com>
2024-10-31 16:31:45 +0530

    route modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Profile from "./pages/Profile";
-import SignIn from "./pages/SignIn";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Profile from "./pages/profile";
+import SignIn from "./pages/signIn";
 import SignUp from "./pages/SignUp";
 
 export default function App() {
-  return <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/about" element={<About />} />
-    <Route path="/profile" element={<Profile />} />
-    <Route path="/sign-in" element={<SignIn />} />
-    <Route path="/sign-up" element={<SignUp />} />
-  </Routes>
-  </BrowserRouter>
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+  
 }
